444a58f4f84c58bf1fd5c8c4ba915e6ad1e17374
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-11-07 22:38:23 -0500

    minor

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -219,4 +219,35 @@ router.delete(
   }
 );
 
+//Unneccissay spelling error and extra delete
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        //Check to see if the comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexist: 'Comment does not exist' });
+        }
+
+        // Get the remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //Remove
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
